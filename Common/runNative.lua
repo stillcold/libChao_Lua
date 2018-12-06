cade7a3d5a79a0e073fc926e1ca579631798ad0b
Chao.guan <mr_kernel@163.com>
2018-12-06 14:37:47 +0800

    comment: Add comment for run native method

--- Common/runNative.lua
+++ Common/runNative.lua
@@ -1,5 +1,15 @@
 -- call native method from lua
 
+-- Sample code:
+-- 
+-- local allFiles = RunNativeMethod("dir")
+-- for line in allFiles:lines() do
+-- 	print(line)
+-- end
+
+-- This will return a user data which can be access in lua code by lines method,
+-- See sample code above for more.
+
 function RunNativeMethod(cmd)
 	return io.popen(cmd)
 end
