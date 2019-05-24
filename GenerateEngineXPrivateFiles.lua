bd21d39c11dd5c793b53fe9fdfe49db3c3dbdef5
stillcold <mr_kernel@163.com>
2019-05-24 17:26:31 +0800

    code: modify modify entry logic

--- GenerateEngineXPrivateFiles.lua
+++ GenerateEngineXPrivateFiles.lua
@@ -1,22 +1,19 @@
 require "Common/class"
 
-local function runDemo_FolderEncode()
+local function runDemo_FolderEncode(bEncode)
 	local encoder = require "Encode/FolderEncoder"
 	local key = "xiaoxiao"
-	-- local inputDirName = "D:/Projects/x/deploy/encoded-codemgr-src/x_code_deploy_dir/keywords"
-	local inputDirName = "D:/Projects/x/deploy/encoded-codemgr-src/x_code_deploy_dir"
+	local inputDirName = "E:/J/PhpStudyDir/x_code_deploy_dir"
 	local includeFilter = ".lua"
 	local excludeFilter = ".sherry"
 	local encodeLen = 100
 	local algrithmVersion = 1
-	local outputFileName = nil
+	local outputFileName = "D:/Projects/x/deploy/encoded-codemgr-src/x_code_deploy_dir.sherryd"
 	local bHandleSubFolder = true
 
-	local bEncode = 10
-	if bEncode == 1 then
+	if bEncode == 1 or bEncode == true then
 		encoder:EncodeFolderBinary(key, inputDirName, includeFilter, excludeFilter, encodeLen, algrithmVersion, outputFileName, bHandleSubFolder)
 	else
-		-- local inputFileName = "D:/Projects/x/deploy/encoded-codemgr-src/x_code_deploy_dir/keywords.sherryd"
 		local inputFileName = "D:/Projects/x/deploy/encoded-codemgr-src/x_code_deploy_dir.sherryd"
 		outputFileName = "D:/Projects/Test/out"
 		encoder:DecodeFolderBinary(key, inputFileName, encodeLen, algrithmVersion, outputFileName)
@@ -24,4 +21,4 @@ local function runDemo_FolderEncode()
 	
 end
 
-runDemo_FolderEncode()
+runDemo_FolderEncode(1)
